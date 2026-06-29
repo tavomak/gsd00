87779af696a7823d0b473dcbe318fe37c1ed43dd
Gustavo Rivero <tavomak@gmail.com>
2026-06-29 14:58:47 -0400

    fix: update

--- utils/index.js
+++ utils/index.js
@@ -1,8 +1,5 @@
 export * from './lib/api';
 export * from './constants';
 export * from './helpers';
-<<<<<<< Updated upstream
-=======
 export * from './queries';
 export * from './seo';
->>>>>>> Stashed changes
